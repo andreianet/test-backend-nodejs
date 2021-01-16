8d65ccbd1b556a85cf5db542faa0423aaa714c0d
andreianet <andreia.netsys@hotmail.com>
2021-01-16 13:26:21 -0300

    Atualiado Readme, modificado Controller e Routes

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,5 +1,5 @@
 const productsCollection = require('../models/productSchema');
-const {model} = require('mongoose')
+//const {model} = require('mongoose')
 
 
 //Obtendo todos o produtos registrados 
@@ -40,14 +40,14 @@ const updateProducts = (req, res) => {
     const productBody = req.body
     const novo = {new: true} //retorna valor modificado
 
-    product.productsCollection.findByIdAndUpdate(
+    productsCollection.findByIdAndUpdate(
         id,
         {$set: productBody}, //com os colchetes não funciona
         novo,
         (error, product) => {
             if (error){
                 return res.status(500).send(error)
-            }else if(musics){
+            }else if(product){
                 return res.status(200).send({
                     message: 'Produto Atualizado com Sucesso',
                     product
@@ -63,18 +63,75 @@ const updateProducts = (req, res) => {
 //Excluir produto
 const delProduct = (req, res) => {
     const id = req.params.id
-    product.productsCollection.findByIdAndDelete(id,(error, product) =>{
+    productsCollection.findByIdAndDelete(id,(error, product) =>{
         if (error) {
-            return res.status(500).send(error)
+            return res.status(500).send({
+                message: 'Ocorreu um error, verificar!',
+                error
+            })
         }else{
             if (product) {
                 return res.status(200).send("Produto Deletado com Sucesso!")
             }else{
-                return res.status(404)
+                return res.status(400)
             }
         }
     })
 }
+
+//Busca por ID
+const ProductById = (req, res) => {
+    const id = req.params.id
+    productsCollection.findById(id,(error, product) => {
+        if (error) {
+            return res.status(500).send(error);            
+        }else{
+            if (product){
+                res.status(200).send(product)
+            }else{
+                return res.status(404).send('Produto não encontrado: ( ')
+            }
+        }
+    })
+}
+
+//Filtrar por descricao do Produto 
+const getProductPorNome = (req, res) => {
+    const descricao = req.params.descricao
+    productsCollection.filter({product: descricao},(error, product) => {
+        if (error) {
+            return res.status(500).send(error);
+        }else if(descricao == ''){
+            return res.status(404).send('Produto não encontrado!')
+        }else{
+            return res.status(200).send({
+                message: 'Produto encontrado!',
+                product
+            })
+        }
+    })
+    
+}
+
+//Filtrar por categoria
+const getProductPorCategoria = (req, res) => {
+    const categoria = req.params.categoria
+    productsCollection.filter({product: categoria},(error, product) => {
+        if (error) {
+            return res.status(500).send(error);
+        }else if(categoria == ''){
+            return res.status(404).send('Categoria não encontrada!')
+        }else{
+            return res.status(200).send({
+                message: 'Categoria encontrada!',
+                product
+            })
+        }
+    })
+    
+}
+
+
 
 
 
@@ -82,5 +139,8 @@ module.exports = {
     addProduct,
     allProducts,
     updateProducts,
-    delProduct
+    delProduct,
+    ProductById,
+    getProductPorNome,   
+    getProductPorCategoria
 }
--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -5,11 +5,20 @@ const controller = require('../controllers/productsController')
 //@Description: Get 
 router.get('/', controller.allProducts)
 
+//Por id
+router.get('/:id', controller.ProductById)
+
+//busca por nodemon
+router.get('/:nome', controller.getProductPorNome)
+
 //@Description: Post - Criar novos product
 router.post('/registrar', controller.addProduct)
 
 //@Description: PUT - Atualizar produto por ID
-router.put('/{id}', controller.updateProducts)
+router.put('/:id', controller.updateProducts)
+
+//@Description: DELETE - Deletar um produto por ID
+router.delete('/:id', controller.delProduct)
 
 
 module.exports = router
