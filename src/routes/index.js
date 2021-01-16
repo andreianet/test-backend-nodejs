1609b4b1885eb4c0145078e5c1b05c2fa1d998c5
andreianet <andreia.netsys@hotmail.com>
2021-01-16 14:42:18 -0300

    alterado arquivo

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,8 @@ const router = express.Router()
 
 router.get('/', function (req, res) {
     res.status(200).send({
-        titulo: "Products - AnotaAí",
+        titulo: "Cátalogo de Produtos - Desafio - AnotaAí",
         version: "1.0.0"
     })
 })
-
 module.exports = router
